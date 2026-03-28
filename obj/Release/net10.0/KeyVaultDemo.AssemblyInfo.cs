0ef5b8539f1c8300f4f4d6b434671d57271f7987
Hanita Melin <hanita.2010@live.se>
2026-03-28 11:00:25 +0100

    Production deployment complete - Key Vault and MongoDB working

--- obj/Release/net10.0/KeyVaultDemo.AssemblyInfo.cs
+++ obj/Release/net10.0/KeyVaultDemo.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeyVaultDemo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3df87bf962e70e3103a1535eff9cb23381d451c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeyVaultDemo")]
